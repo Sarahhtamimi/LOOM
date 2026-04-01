6bffb39df29b7d9002a35b0f94029a5a244b58ad
Sarahhtamimi <sarah1tamimi@gmail.com>
2026-04-01 20:31:19 +0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -11,6 +11,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <body>
         <h1>hello<h1>
         <?php
+        echo "hi";
         // put your code here
         ?>
     </body>
